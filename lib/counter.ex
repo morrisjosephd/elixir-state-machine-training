8fb071c43c840f630f8ed0e62822a8a68002d2e9
Joe Morris <joe@scriptdrop.co>
2020-01-17 11:37:33 -0500

    getter and setter methods for counter to worker

--- lib/counter.ex
+++ lib/counter.ex
@@ -1,3 +1,9 @@
 defmodule Counter do
-  
+  def read do
+    GenServer.call(Counter.Worker, :read)
+  end
+
+  def increment do
+    GenServer.cast(GenServer.Worker, :inc)
+  end
 end
--- lib/counter/worker.ex
+++ lib/counter/worker.ex
@@ -1,7 +1,22 @@
 defmodule Counter.Worker do
-    use GenServer
+  use GenServer
 
-    def start_link(args) do
-        GenServer.start_link(__MODULE__, args, name: __MODULE__)
-    end
+  # tells genserver to start
+  def start_link(args) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    {:ok, 0}
+  end
+
+  def handle_call(:read, _from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast(:inc, state) do
+    state = state + 1
+
+    {:noreply, state}
+  end
 end
